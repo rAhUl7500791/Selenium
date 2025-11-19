f9878ea9df4dd6ff07ff9be19d256b76faada581
rahul bisht <rahulbisht1009@gmail.com>
2025-11-19 21:32:32 +0530

    'seventhcommit'

--- src/test/java/listeners/ExtentReporterNG.java
+++ src/test/java/listeners/ExtentReporterNG.java
@@ -5,20 +5,20 @@ import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 import com.aventstack.extentreports.reporter.configuration.Theme;
 
 public class ExtentReporterNG {
+
     static ExtentReports extent;
 
     public static ExtentReports getReporterObject() {
 
-//        String path = System.getProperty("user.dir") + "/reports/index.html";
+        // Report will be generated inside target folder
         ExtentSparkReporter reporter = new ExtentSparkReporter("target/extent-report.html");
 
-        // ---------- Report UI Settings ----------
-        reporter.config().setTheme(Theme.DARK);                       // BEAUTIFUL DARK THEME
-        reporter.config().setReportName("E2E Automation Report");     // Top header name
-        reporter.config().setDocumentTitle("Test Execution Report");  // Browser tab title
-        reporter.config().setTimeStampFormat("dd/MM/yyyy HH:mm:ss");  // Better timestamps
-        reporter.config().setEncoding("UTF-8");                       // For symbols, fonts
-        //reporter.config().setCss("body { font-size: 14px; }");      // OPTIONAL custom CSS
+        // ---------- UI Settings ----------
+        reporter.config().setTheme(Theme.DARK);
+        reporter.config().setReportName("E2E Automation Report");
+        reporter.config().setDocumentTitle("Test Execution Report");
+        reporter.config().setTimeStampFormat("dd/MM/yyyy HH:mm:ss");
+        reporter.config().setEncoding("UTF-8");
 
         extent = new ExtentReports();
         extent.attachReporter(reporter);
